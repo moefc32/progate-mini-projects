f5d4181d4fdf1a73155f87c40f626efc7e0e6671
Faizal Chan <moefc32@gmail.com>
2020-09-04 18:49:00 +0700

    update

---
+++ project/03/res/script.js
@@ -0,0 +1,27 @@
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    console.log('Full name : ' + profile.getName());
+    console.log('E-Mail : ' + profile.getEmail())
+
+    var x = document.getElementsByClassName('logged');
+    var i;
+    for (i = 0; i <= x.length; i++) {
+        x[0].classList.remove('logged');
+    }
+}
+
+function onSignOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function() {
+        console.log('User signed out.');
+        hideDashboard();
+    });
+}
+
+function hideDashboard() {
+    var x = document.getElementsByClassName('onlylogged');
+    var i;
+    for (i = 0; i <= x.length; i++) {
+        x[i].classList.add('logged');
+    }
+}
